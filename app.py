8c63cbcd11932dbd3092ba73a4533282d9859115
not12nanna <88994927+not12nanna@users.noreply.github.com>
2021-09-27 08:58:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
 ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
 
-lineaccesstoken = ''
+lineaccesstoken = '+hnibw6e3gZeNe7mNF29cwekFXZ7I0yFvPwEvXCqIjZ3YbQIt/UcLqOSsXgmzmdFoc6MbtMfvoUKPyqk6VBTEAsAkKPWPqB7z0/DQgSNTuMc4RMzxJ+7ORoUSHIbipJ+U9noPRcZLt5wO22ymo0SmwdB04t89/1O/w1cDnyilFU='
 
 line_bot_api = LineBotApi(lineaccesstoken)
 
